d26ab3973f1e0eb473ef178db0885d8a40589062
Erin Miller <erinemillerr@gmail.com>
2018-09-19 16:08:25 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Catch Up Exercises
+## Navigation built at run-time from a JS array
+
+In the main.js file, you'll find an array called <code>navigation</code>.
+
+In the index.html file, you'll find a header already set up, with navigation links in it.
+
+Your job is to remove those example links (delete them from the html, or comment them), and instead, use JS to generate those elements and place them in the DOM, using the data from the <code>navigation</code> array.
+
+This is how many websites actually work- they don't have the navigation links actually hard-coded into the HTML, they retreive the data about the navigation links, and generate them when they're needed.
+
+Good luck!
---
+++ src/js/main.js
@@ -0,0 +1,92 @@
+console.log(`Hello World from app.js! 
+Change this message, and make sure it changes in the browser 
+to verify that you're working in the right files.`)
+
+let navigation = [
+	{
+		name: "About Us",
+		link: "about.html"
+	},
+	{
+		name: "Our Services",
+		link: "services.html"
+	},
+	{
+		name: "Testimonials",
+		link: "testimonials.html"
+	},
+	{
+		name: "Contact",
+		link: "contact.html"
+	},
+	{
+		name: "Test",
+		link: "test.html"
+	},
+]
+
+
+for (var i = 0; i < navigation.length; i++) {
+	console.log( navigation[i].link )
+	
+	var newli = document.createElement("li");
+	document.querySelector("ul").appendChild(newli);
+	
+	var atag = document.createElement("a");
+	newli.appendChild(atag);
+
+	atag.setAttribute('href', navigation[i].link);
+	atag.innerHTML = navigation[i].name;
+}
+
+// var nav = document.querySelector("nav ul"),
+//     navToggle = document.querySelector("nav .skip");
+// if (navToggle) {
+//   navToggle.addEventListener("click",
+//   function(e) {
+//   if (nav.className == "open") {
+//     nav.className = "";
+//   } else {
+//     nav.className = "open";
+//   }
+//   e.preventDefault();
+//   }, false);
+// }
+
+var nav = document.querySelector("nav");
+var hamburger = document.querySelector(".hamburger");
+console.log(hamburger)
+
+hamburger.addEventListener('click', function(e){
+	nav.classList.toggle('active');
+})
+
+// navigation.addEventListener('click', function(e){
+// hamburger.classList.toggle('active');
+// e.preventDefault();
+// }, false);
+
+// when the user clicks the hamburger, the navigation appears
+
+
+
+// take that data and make a navigation out of it
+// take that data and make all the elements we need for a navigation and display them
+// take that data and make all the elements we need for a navigation and put them inside the navigation UL
+// take that data and make LIs and A elements we need for a navigation and put them inside the navigation UL
+// take that data and make LIs and A elements and give them text and href from the data and put them inside the navigation UL
+
+
+// find an element so we can do stuff to it later
+// innerHTML to set the text
+// appendChild for putting elements in elements
+// setAttribute to set attributes like href on elements
+
+
+
+// user clicks hamburger icon, nav bar pops up
+// user clicks hamburger icon, nav bar becomes visible
+
+// techniques
+// addEventListener
+// .style to the element
